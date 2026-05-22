83a0c75ae1b2a24d0af5474436f5ce7816fb30c8
annaniedzialek <ania.niedzialek@sjsu.edu>
2026-05-22 14:29:17 -0700

    three questions added

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function openWindow() {
+    document.getElementById("popupWindow").hidden = false;
+    document.getElementById("popupOverlay").hidden = false;
+}
+
+function closeWindow() {
+    document.getElementById("popupWindow").hidden = true;
+    document.getElementById("popupOverlay").hidden = true;
+
+}
