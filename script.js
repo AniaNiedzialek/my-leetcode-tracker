aa16644c68c4c65a175694dca5a06529098929ee
annaniedzialek <ania.niedzialek@sjsu.edu>
2026-05-22 18:38:12 -0700

    changed design of binary tree and html - added 8 patterns

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
+const popup = document.getElementById("popup");
+const popupTitle = document.getElementById("popupTitle");
+const popupMessage = document.getElementById("popupMessage");
+const closePopupBtn = document.getElementById("closePopupBtn");
 
-function openWindow() {
-    document.getElementById("popupWindow").hidden = false;
-    document.getElementById("popupOverlay").hidden = false;
-}
+const openPopupButtons = document.querySelectorAll(".open-popup-btn");
 
-function closeWindow() {
-    document.getElementById("popupWindow").hidden = true;
-    document.getElementById("popupOverlay").hidden = true;
+openPopupButtons.forEach(function (button) {
+  button.addEventListener("click", function () {
+    const title = button.getAttribute("data-title");
+    const message = button.getAttribute("data-message");
 
-}
+    popupTitle.textContent = title;
+    popupMessage.textContent = message;
+
+    popup.style.display = "block";
+  });
+});
+
+closePopupBtn.addEventListener("click", function () {
+  popup.style.display = "none";
+});
+
+window.addEventListener("click", function (event) {
+  if (event.target === popup) {
+    popup.style.display = "none";
+  }
+});
